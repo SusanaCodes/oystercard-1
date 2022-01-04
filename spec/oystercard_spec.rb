4bb2f647cff7a568ca4484c11ea1ce234a078f55
Jenny <j.e.robson@live.com>
2022-01-04 16:12:26 +0000

    passing touch out test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -33,4 +33,10 @@ describe '#top_up' do
     subject.touch_in
     expect(subject).to be_in_journey
   end
+
+  it 'can touch out' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
 end
